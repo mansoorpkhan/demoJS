d8578ddf2f46b34f30fa1af2beec604e943d77da
mansoorpkhan <mansoorkhan2004@gmail.com>
2025-12-02 23:39:54 +0530

    Task 2

---
+++ Task2/app.js
@@ -0,0 +1,119 @@
+// // 1. Find the sum of all even numbers between 1 and 100
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log("Sum =", sum);
+
+
+// // 2. Print all numbers divisible by 7 between 1 and 100
+
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// // 3. Count how many digits are in a given number**
+
+
+// let num = 98765;
+// let count = num.toString().length;
+
+// console.log("Digits =", count);
+
+// // 4. Print all characters of a string one by one
+
+
+// let str = "hello";
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+// // 5. Count how many odd numbers are present in an array
+
+
+// let arr = [1, 4, 7, 9, 12];
+// let count = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 !== 0) {
+//     count++;
+//   }
+// }
+
+// console.log("Odd numbers count =", count);
+
+
+// // 6. Count how many times a specific character appears in a string
+
+
+// let str = "javascript";
+// let ch = "a";
+// let cout = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] === ch) {
+//     cout++;
+//   }
+// }
+
+// console.log("Count =", count);
+
+
+// // 7. Check if a number is divisible by 10
+
+
+// let num = 50;
+
+// if (num % 10 === 0) {
+//   console.log("Divisible by 10");
+// } else {
+//   console.log("Not divisible");
+// }
+
+
+// // 8. Print all even numbers from an array
+
+
+// let arr = [2, 5, 8, 11, 14];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 9. Print numbers from 1 to N in reverse
+
+
+// let N = 10;
+
+// for (let i = N; i >= 1; i--) {
+//   console.log(i);
+// }
+
+
+// // 10. Count how many elements in an array are greater than 50
+
+
+// let arr = [10, 60, 75, 20, 100];
+// let count = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 50) {
+//     count++;
+//   }
+// }
+
+// console.log("Greater than 50 =", count);
+
+
